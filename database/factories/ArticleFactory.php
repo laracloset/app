44d17f9050dc2afc0dbd5f3d1ea475c8a49fa42b
tsmsogn <tsmsogn@gmail.com>
2018-12-04 14:28:56 +0900

    Update ArticleFactory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,8 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(\App\Article::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->title,
+        'slug' => $faker->unique()->slug,
+        'body' => $faker->sentence,
+        'state' => \App\Article::PUBLISHED
     ];
 });
