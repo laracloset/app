8a3bb174c7e2f52059fc2f80754510439028f0de
tsmsogn <tsmsogn@gmail.com>
2018-12-04 14:30:22 +0900

    Add article pages

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
@@ -13,72 +13,21 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $articles = Article::query()->paginate();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return view('article.index', compact('articles'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
-    }
+        $article = Article::query()->findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('article.show', compact('article'));
     }
 }
--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -2,19 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\Article;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ArticleTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
      * @return void
      */
-    public function testExample()
+    public function testGetIndex()
     {
-        $this->assertTrue(true);
+        $this->get('/articles')
+            ->assertOk();
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetDetail()
+    {
+        $article = factory(Article::class)->create();
+
+        $this->get('/articles/' . $article->id)
+            ->assertOk();
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetDetailWithInvalidId()
+    {
+        $this->get('/articles/0')
+            ->assertNotFound();
     }
 }
