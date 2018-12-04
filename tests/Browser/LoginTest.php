9c7b3d0f4fba4dfcd3246832d61a9ca95a1bb06d
tsmsogn <tsmsogn@gmail.com>
2018-12-04 09:46:22 +0900

    Add a login test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,22 +2,30 @@
 
 namespace Tests\Browser;
 
-use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /**
-     * A Dusk test example.
-     *
      * @return void
+     * @throws \Throwable
      */
-    public function testExample()
+    public function testLogin()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home');
         });
     }
 }
