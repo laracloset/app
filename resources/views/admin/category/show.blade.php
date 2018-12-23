ecc3457e13e6763157a1ae86cb7688f5042d73b6
tsmsogn <tsmsogn@gmail.com>
2018-12-24 00:06:39 +0900

    Drop div.container

--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -3,33 +3,31 @@
 @section('title', 'Categories')
 
 @section('content')
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-                View Category
-            </div>
-            <div class="card-body">
-                <dl>
-                    <dt>Id</dt>
-                    <dd>{{ $category->id }}</dd>
-                </dl>
-                <dl>
-                    <dt>Name</dt>
-                    <dd>{{ $category->name }}</dd>
-                </dl>
-                <dl>
-                    <dt>Slug</dt>
-                    <dd>{{ $category->slug }}</dd>
-                </dl>
-                <dl>
-                    <dt>Created At</dt>
-                    <dd>{{ $category->created_at }}</dd>
-                </dl>
-                <dl>
-                    <dt>Updated At</dt>
-                    <dd>{{ $category->updated_at }}</dd>
-                </dl>
-            </div>
+    <div class="card">
+        <div class="card-header">
+            View Category
+        </div>
+        <div class="card-body">
+            <dl>
+                <dt>Id</dt>
+                <dd>{{ $category->id }}</dd>
+            </dl>
+            <dl>
+                <dt>Name</dt>
+                <dd>{{ $category->name }}</dd>
+            </dl>
+            <dl>
+                <dt>Slug</dt>
+                <dd>{{ $category->slug }}</dd>
+            </dl>
+            <dl>
+                <dt>Created At</dt>
+                <dd>{{ $category->created_at }}</dd>
+            </dl>
+            <dl>
+                <dt>Updated At</dt>
+                <dd>{{ $category->updated_at }}</dd>
+            </dl>
         </div>
     </div>
 @endsection
