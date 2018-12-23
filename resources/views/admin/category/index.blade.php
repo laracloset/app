dc16039e8b809f128e6202e2710eecec4881cbe6
tsmsogn <tsmsogn@gmail.com>
2018-12-23 23:55:31 +0900

    Drop div.container

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -3,46 +3,44 @@
 @section('title', 'Categories')
 
 @section('content')
-    <div class="container">
-        <a class="btn btn-primary mb-2" href="{{ route('categories.create') }}" role="button">Create Category</a>
-        <div class="card">
-            <div class="card-header">
-                Categories
-            </div>
-            <div class="card-body">
-                <table class="table">
-                    <thead>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Created At</th>
-                    <th>Actions</th>
-                    </thead>
-                    <tbody>
-                    @foreach($categories as $category)
-                        <tr>
-                            <td>{{ $category->id }}</td>
-                            <td>{{ $category->name }}</td>
-                            <td>{{ $category->created_at }}</td>
-                            <td>
-                                <a class="btn btn-primary" href="{{ route('categories.show', $category->id) }}"
-                                   role="button">View</a>
-                                <a class="btn btn-primary"
-                                   href="{{ route('categories.edit', $category->id) }}"
-                                   role="button">Edit</a>
-                                <form action="{{ route('categories.destroy', $category->id)}}" method="post"
-                                      class="d-inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger" type="submit" dusk="delete">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+    <a class="btn btn-primary mb-2" href="{{ route('categories.create') }}" role="button">Create Category</a>
+    <div class="card">
+        <div class="card-header">
+            Categories
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Created At</th>
+                <th>Actions</th>
+                </thead>
+                <tbody>
+                @foreach($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>{{ $category->created_at }}</td>
+                        <td>
+                            <a class="btn btn-primary" href="{{ route('categories.show', $category->id) }}"
+                               role="button">View</a>
+                            <a class="btn btn-primary"
+                               href="{{ route('categories.edit', $category->id) }}"
+                               role="button">Edit</a>
+                            <form action="{{ route('categories.destroy', $category->id)}}" method="post"
+                                  class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit" dusk="delete">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
 
-                {{ $categories->links() }}
-            </div>
+            {{ $categories->links() }}
         </div>
     </div>
 @endsection
