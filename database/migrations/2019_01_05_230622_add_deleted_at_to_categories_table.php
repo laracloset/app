7038a4758d7677cf2fd40d5888be017b7eecfb85
tsmsogn <tsmsogn@gmail.com>
2019-01-06 09:29:09 +0900

    Setup category schema

--- database/migrations/2019_01_05_230622_add_deleted_at_to_categories_table.php
+++ database/migrations/2019_01_05_230622_add_deleted_at_to_categories_table.php
@@ -14,7 +14,7 @@ class AddDeletedAtToCategoriesTable extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
@@ -26,7 +26,7 @@ class AddDeletedAtToCategoriesTable extends Migration
     public function down()
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 }
