92d3ec0bdda23227e144742733497c8ed3a8825c
tsmsogn <tsmsogn@gmail.com>
2019-01-09 13:47:40 +0900

    Set up AssetFactory

--- database/factories/AssetFactory.php
+++ database/factories/AssetFactory.php
@@ -1,9 +1,18 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(\App\Asset::class, function (Faker $faker) {
+    $file = UploadedFile::fake()->image('avatar.jpg');
+    $path = $file->store('assets');
+
     return [
-        //
+        'model' => 'Asset',
+        'name' => $file->getClientOriginalName(),
+        'type' => $file->getMimeType(),
+        'size' => $file->getSize(),
+        'path' => $path,
     ];
 });
