2f5acd07d5c62cbc59c97992bb24fa98c3927613
tsmsogn <tsmsogn@gmail.com>
2019-01-10 13:19:30 +0900

    Use sentence

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Article::class, function (Faker $faker) {
     return [
-        'title' => $faker->word(),
+        'title' => $faker->sentence(),
         'slug' => $faker->unique()->slug(),
         'body' => $faker->sentence(100),
         'state' => \App\Article::PUBLISHED
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Category::class, function (Faker $faker) {
     return [
-        'name' => $faker->word(),
+        'name' => $faker->sentence(),
         'slug' => $faker->unique()->slug(),
     ];
 });
