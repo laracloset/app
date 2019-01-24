2a6695d89572e0964de49b72cfe154cb578ec8cd
tsmsogn <tsmsogn@gmail.com>
2019-01-24 17:10:03 +0900

    Fix failed tests

--- tests/Browser/Admin/CategoryTest.php
+++ tests/Browser/Admin/CategoryTest.php
@@ -41,7 +41,7 @@ class CategoryTest extends AdminDuskTestCase
 
         $this->browse(function (Browser $browser) use ($categories) {
             $browser->visit('/admin/categories')
-                ->assertDontSeeLink('2')
+                ->assertDontSeeLink('›')
                 ->assertSee($categories[0]->id)
                 ->assertSee($categories[19]->id);
         });
