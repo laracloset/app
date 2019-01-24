9b770591dd28eccf00363512538448a8abbc3d24
tsmsogn <tsmsogn@gmail.com>
2019-01-24 11:15:27 +0900

    Use DatabaseMigrations Trait

--- tests/Feature/Admin/AdminTestCase.php
+++ tests/Feature/Admin/AdminTestCase.php
@@ -4,10 +4,12 @@ namespace Tests\Feature\Admin;
 
 
 use App\Admin;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 abstract class AdminTestCase extends TestCase
 {
+    use DatabaseMigrations;
 
     public function setUp()
     {
