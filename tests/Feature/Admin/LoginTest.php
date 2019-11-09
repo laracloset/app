3ca6aaa2210796fdabdc4b4c8473878344b78f39
tsmsogn <tsmsogn@gmail.com>
2019-11-09 10:13:03 +0900

    Disable inactive admin to login

---
+++ tests/Feature/Admin/LoginTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Admin;
+use App\Enums\LoginStatusType;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @return void
+     */
+    public function testLogin()
+    {
+        $admin = factory(Admin::class)->create([
+            'password' => Hash::make('secret')
+        ]);
+
+        $this->post('/admin/login', [
+            'email' => $admin,
+            'password' => 'secret',
+        ])->assertRedirect('/');
+
+        $this->assertAuthenticated('admin');
+    }
+
+    /**
+     * @return void
+     */
+    public function testLoginWithInactive()
+    {
+        $admin = factory(Admin::class)->create([
+            'password' => Hash::make('secret'),
+            'active' => LoginStatusType::INACTIVE
+        ]);
+
+        $this->post('/admin/login', [
+            'email' => $admin,
+            'password' => 'secret',
+        ]);
+
+        $this->assertGuest();
+    }
+}
