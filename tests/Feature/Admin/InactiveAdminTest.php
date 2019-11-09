874f9a88c208f55d8febffa78aa86f69d5f45508
tsmsogn <tsmsogn@gmail.com>
2019-11-09 16:48:00 +0900

    Refresh DB

--- tests/Feature/Admin/InactiveAdminTest.php
+++ tests/Feature/Admin/InactiveAdminTest.php
@@ -4,10 +4,13 @@ namespace Tests\Feature\Admin;
 
 use App\Admin;
 use App\Enums\LoginStatusType;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class InactiveAdminTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * @return void
      */
