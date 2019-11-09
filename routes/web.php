47a9cfab51648fe5ae02f0aca3fbfce664e6c33c
tsmsogn <tsmsogn@gmail.com>
2019-11-09 15:02:18 +0900

    Use name('admin.')

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/articles', 'ArticleController@index');
 Route::get('/articles/{article}', 'ArticleController@show');
 
-Route::prefix('admin')->namespace('Admin')->middleware('admin_auth:admin')->group(function () {
+Route::prefix('admin')->namespace('Admin')->middleware('admin_auth:admin')->name('admin.')->group(function () {
     Route::resource('articles', 'ArticleController');
 
     Route::resource('assets', 'AssetController');
@@ -38,15 +38,15 @@ Route::prefix('admin')->namespace('Admin')->middleware('admin_auth:admin')->grou
     Route::get('/home', 'HomeController@index')->name('admin.home');
 });
 
-Route::prefix('admin')->namespace('Admin')->group(function () {
+Route::prefix('admin')->namespace('Admin')->name('admin.')->group(function () {
     // Authentication Routes...
-    $this->get('login', 'Auth\LoginController@showLoginForm')->name('admin.login');
+    $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
     $this->post('login', 'Auth\LoginController@login');
-    $this->post('logout', 'Auth\LoginController@logout')->name('admin.logout');
+    $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
     // Password Reset Routes...
-    $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-    $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-    $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('admin.password.reset');
-    $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('admin.password.update');
+    $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 });
